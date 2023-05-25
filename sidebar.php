958147b91185a5aff9415d5683b5c86313322028
721377 <nonelabide@gmail.com>
2023-05-25 11:38:35 +0000

    sidebar labide

---
+++ sidebar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="css/sidebar.css">
+    <title>login</title>
+</head>
+<body>
+<div class="nav">
+
+<nav>
+    <div class="search">
+       <div class="search-icon"><i class="bi bi-search"></i><input type="text" name="" id="" placeholder="chercher"></div>
+        
+        <label for="">chercher par Nom</label>
+        <input type="submit" class="sub-chercher" value="chercher">
+    </div>
+    
+    <ul>
+    <h4>listes des retraites</h4>
+        <li class="one"><a href=""><i class="bi bi-person-fill"></i>presone1</a> <p>2023</p></li>
+        <li><a href=""><i class="bi bi-person-fill"></i>presone1</a> <p>2023</p></li>
+        <li><a href=""><i class="bi bi-person-fill"></i>presone1</a> <p>2023</p></li>
+        <li><a href=""><i class="bi bi-person-fill"></i>presone1</a> <p>2023</p></li>
+        <li><a href=""><i class="bi bi-person-fill"></i>presone1</a> <p>2023</p></li>
+        <li><a href=""><i class="bi bi-person-fill"></i>presone1</a> <p>2023</p></li>
+        <li><a href=""><i class="bi bi-person-fill"></i>presone1</a> <p>2023</p></li>
+    </ul>
+</nav>
+
+</div>
+</body>
+</html>
