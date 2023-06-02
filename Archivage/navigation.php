43a2d898b0cc665af6d1e958f4b1638e5f9ee107
assia charafi <134143091+Assiiaa@users.noreply.github.com>
2023-06-02 09:44:03 +0100

    Add files via upload

---
+++ Archivage/navigation.php
@@ -0,0 +1,12 @@
+
+<div class="navigation">
+    <div class="nav-pages">
+    <ul class="uli" >
+        <li><a href="list-prof.php">liste</a></li>
+        <li><a href="ajouter-prof.php">ajouter prof</a></li>
+        <li><a href="liste-absence.php">absence </a></li>
+        <li><a href="liste-conges.php">archifage</a></li>
+        <li><a href="">Rédaction</a></li>
+    </ul>
+    </div>
+</div>
---
+++ Archivage/sidebar-archivage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="css/sidebar-archivage.css">
+    <title>login</title>
+</head>
+<body>
+<div class="nav">
+
+<nav>
+    <div class="search">
+       <div class="search-icon"><i class="bi bi-search"></i><input type="text" name="" id="" placeholder="chercher"></div>
+        
+        <label for="">chercher par Nom et Prenom</label>
+        <input type="submit" class="sub-chercher" value="chercher">
+    </div>
+  
+</nav>
+
+</div>
+</body>
+</html>
