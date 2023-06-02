97fcee30feb92b256950a731ac16d95f9e742a27
assia charafi <134143091+Assiiaa@users.noreply.github.com>
2023-06-02 09:39:57 +0100

    Add files via upload

---
+++ Archivage/liste-conges.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstdap-icons@1.10.5/font/bootstdap-icons.css">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!-- font-awesome icon cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/ajouter-prof.css">
+    <link rel="stylesheet" href="css/liste-conges.css">
+    <title>login</title>
+</head>
+<body>
+<?php //include 'header2.php';?> 
+
+<section class="home">
+<?php include 'sidebar-archivage.php';?> 
+
+
+
+<div class="navigation-pages">
+<?php include 'navigation.php'; ?>
+
+<div class="table-box">
+<table>
+<Thead> 
+            <th></th> 
+        <th>قرارات</th>
+        <th>الملاحظات</th>
+        <th>رقم وتاريخ الإرسال إلى الأكاديمية</th>
+        <th>تقرير الفحص المضاد</th>
+        <th>الفحص المضاد</th>
+        <th>الطبيب الذي قدم الفحص</th>
+        <th>تنتهي بتاريخ</th>
+        <th>:مؤرخة في</th>
+        <th>مدتها</th>
+        <th>رقمها</th>
+        <th>الشواهد</th>
+        <th>مقر العمل</th>
+        <th>رقم التأجير</th>
+        <th>الإسم الكامل</th>
+        <th>السلك</th>
+        <th>المؤسسة</th>
+</Thead>
+
+<tbody>
+            <tr>
+            <td>
+            <a href="http://"><i class="bi bi-trash"></i></a> 
+            <a href="http://"><i class="bi bi-pencil"></i></a>
+              </td>
+        
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    </tr>
+
+    <tr>
+    <td>
+    <a href="http://"><i class="bi bi-trash"></i></a> 
+    <a href="http://"><i class="bi bi-pencil"></i></a>
+    </td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    <td>المؤسسة</td>
+    </tr>
+
+</tbody>
+</table>
+</div>
+
+        <div class="submit">
+
+            <div class="del-save">
+                <a class="del" href=""> Imprimer la liste</a>
+                <a class="save" href=""> Ajouter un nouveau enregistrement</a>
+            </div>
+        </div>
+
+   
+</div>
+</section>
+
+
+
+</body>
+</html>
